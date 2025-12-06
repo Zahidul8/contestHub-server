d761a92dddbffa1c2a408a784ce0347b4a3623e8
Your NameZahidul8 <www.zahidul8@gmail.com>
2025-12-06 20:35:13 +0600

    make get api some changes for get data by types

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
 
 // middleware 
@@ -25,38 +25,48 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    
+
     const db = client.db('contest_hub_db');
     const contestsCollection = db.collection('contests')
 
     // creator related contest apis 
 
-    app.get('/contests/all', async(req, res) => {
-      const cursor = contestsCollection.find().sort({'count': -1});
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/contests_all', async (req, res) => {
+      const { type } = req.query;
+
+      const filter = type ? { status: 'approved', contestType: type } : { status: 'approved' };
+      const cursor = contestsCollection.find(filter).sort({ 'count': -1 });
+      const result = await cursor.toArray();
+      res.send(result);
     })
-    app.get('/contests', async(req, res) => {
-        const cursor = contestsCollection.find().sort({'count': -1}).limit(6);
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/contests', async (req, res) => {
+      const cursor = contestsCollection.find().sort({ 'count': -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/contest/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contestsCollection.findOne(query);
+      res.send(result);
     })
 
     app.post('/contest', async (req, res) => {
-        const contestData = req.body;
-        contestData.status = 'pending';
-        const result = await contestsCollection.insertOne(contestData);
-        res.send(result); 
+      const contestData = req.body;
+      contestData.status = 'pending';
+      const result = await contestsCollection.insertOne(contestData);
+      res.send(result);
     })
-    
 
 
-    
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
-   
+
+
   }
 }
 run().catch(console.dir);
