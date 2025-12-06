15d63edc15b5c6e35ce21d5dc4de7b9c4c1b4049
Your NameZahidul8 <www.zahidul8@gmail.com>
2025-12-06 18:58:48 +0600

    make api for get contests data

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
 
+// middleware 
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v0yodsg.mongodb.net/?appName=Cluster0`;
 
 
@@ -22,6 +26,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
+    const db = client.db('contest_hub_db');
+    const contestsCollection = db.collection('contests')
+
+    // creator related contest apis 
+
+    app.get('/contests', async(req, res) => {
+        const cursor = contestsCollection.find().sort({'count': -1}).limit(6);
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/contest', async (req, res) => {
+        const contestData = req.body;
+        contestData.status = 'pending';
+        const result = await contestsCollection.insertOne(contestData);
+        res.send(result); 
+    })
     
 
 
